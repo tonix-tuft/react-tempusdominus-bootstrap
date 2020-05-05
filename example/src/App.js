85d0195e6f6405d2b331012b3303722c9baee612
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-05-05 16:19:43 +0200

    Added fragment links to examples

--- example/src/App.js
+++ example/src/App.js
@@ -20,8 +20,12 @@ export default function App() {
       <hr />
       <div className={styles.components}>
         <div>
-          <h2>i18n</h2>
-          <DateTimePicker locale="ru" />
+          <h2 id="i18n">
+            <a href="#i18n">i18n</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker locale="ru" />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -30,9 +34,15 @@ export default () =>
   <DateTimePicker locale="ru" />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Time Picker</h2>
-          <TimePicker />
+          <h2 id="time-picker">
+            <a href="#time-picker">Time Picker</a>
+          </h2>
+          <div className={styles.picker}>
+            <TimePicker />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { TimePicker } from "react-tempusdominus-bootstrap";
@@ -41,9 +51,15 @@ export default () =>
   <TimePicker />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Date Picker</h2>
-          <DatePicker />
+          <h2 id="date-picker">
+            <a href="#date-picker">Date Picker</a>
+          </h2>
+          <div className={styles.picker}>
+            <DatePicker />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DatePicker } from "react-tempusdominus-bootstrap";
@@ -52,9 +68,15 @@ export default () =>
   <DatePicker />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Month Picker</h2>
-          <MonthPicker />
+          <h2 id="month-picker">
+            <a href="#month-picker">Month Picker</a>
+          </h2>
+          <div className={styles.picker}>
+            <MonthPicker />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { MonthPicker } from "react-tempusdominus-bootstrap";
@@ -63,9 +85,15 @@ export default () =>
   <MonthPicker />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>No Icon</h2>
-          <DateTimePicker noIcon />
+          <h2 id="no-icon">
+            <a href="#no-icon">No Icon</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker noIcon />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -74,9 +102,15 @@ export default () =>
   <DateTimePicker noIcon />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Default Date</h2>
-          <DateTimePicker defaultDate="2020-05-26" />
+          <h2 id="default-date">
+            <a href="#default-date">Default Date</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker defaultDate="2020-05-26" />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -85,9 +119,15 @@ export default () =>
   <DateTimePicker defaultDate="2020-05-26" />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Multidate</h2>
-          <DateTimePicker allowMultidate />
+          <h2 id="multidate">
+            <a href="#multidate">Multidate</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker allowMultidate />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -96,9 +136,15 @@ export default () =>
   <DateTimePicker allowMultidate />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Custom Multidate Separator</h2>
-          <DatePicker allowMultidate multidateSeparator=" · " />
+          <h2 id="custom-multidate-separator">
+            <a href="#custom-multidate-separator">Custom Multidate Separator</a>
+          </h2>
+          <div className={styles.picker}>
+            <DatePicker allowMultidate multidateSeparator=" · " />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DatePicker } from "react-tempusdominus-bootstrap";
@@ -107,9 +153,15 @@ export default () =>
   <DatePicker allowMultidate multidateSeparator=" · " />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Side By Side</h2>
-          <DateTimePicker sideBySide />
+          <h2 id="side-by-side">
+            <a href="#side-by-side">Side By Side</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker sideBySide />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -118,9 +170,15 @@ export default () =>
   <DateTimePicker sideBySide />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>With Seconds</h2>
-          <DateTimePicker format="YYYY-MM-DD HH:mm:ss" />
+          <h2 id="with-seconds">
+            <a href="#with-seconds">With Seconds</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker format="YYYY-MM-DD HH:mm:ss" />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -129,9 +187,15 @@ export default () =>
   <DateTimePicker format="YYYY-MM-DD HH:mm:ss" />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Inline Date Time Picker</h2>
-          <InlineDateTimePicker />
+          <h2 id="inline-date-time-picker">
+            <a href="#inline-date-time-picker">Inline Date Time Picker</a>
+          </h2>
+          <div className={styles.picker}>
+            <InlineDateTimePicker />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { InlineDateTimePicker } from "react-tempusdominus-bootstrap";
@@ -140,8 +204,14 @@ export default () =>
   <InlineDateTimePicker />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Inline Date Time Picker Side By Side</h2>
+          <h2 id="inline-date-time-picker-side-by-side">
+            <a href="#inline-date-time-picker-side-by-side">
+              Inline Date Time Picker Side By Side
+            </a>
+          </h2>
           <div className={styles.inlinePicker}>
             <InlineDateTimePicker sideBySide />
           </div>
@@ -153,15 +223,21 @@ export default () =>
   <InlineDateTimePicker sideBySide />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Disabled Dates</h2>
-          <DateTimePicker
-            disabledDates={[
-              moment("2020-05-07"),
-              new Date(2020, 5 - 1, 21),
-              "2020-05-22 00:53",
-            ]}
-          />
+          <h2 id="disabled-dates">
+            <a href="#disabled-dates">Disabled Dates</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker
+              disabledDates={[
+                moment("2020-05-07"),
+                new Date(2020, 5 - 1, 21),
+                "2020-05-22 00:53",
+              ]}
+            />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 6-12">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -178,9 +254,15 @@ export default () => (
 );`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Disabled Days Of Week</h2>
-          <DateTimePicker daysOfWeekDisabled={[0, 6]} />
+          <h2 id="disabled-days-of-week">
+            <a href="#disabled-days-of-week">Disabled Days Of Week</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker daysOfWeekDisabled={[0, 6]} />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -189,21 +271,29 @@ export default () =>
   <DateTimePicker daysOfWeekDisabled={[0, 6]} />;`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Linked Pickers</h2>
-          <DateTimePicker
-            pickerRef={linkedPicker1Ref}
-            onChange={e => {
-              linkedPicker2Ref.current.datetimepicker("minDate", e.date);
-            }}
-          />
-          <DateTimePicker
-            useCurrent={false}
-            pickerRef={linkedPicker2Ref}
-            onChange={e => {
-              linkedPicker1Ref.current.datetimepicker("maxDate", e.date);
-            }}
-          />
+          <h2 id="linked-pickers">
+            <a href="#linked-pickers">Linked Pickers</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker
+              pickerRef={linkedPicker1Ref}
+              onChange={e => {
+                linkedPicker2Ref.current.datetimepicker("minDate", e.date);
+              }}
+            />
+          </div>
+          <div className={styles.picker}>
+            <DateTimePicker
+              useCurrent={false}
+              pickerRef={linkedPicker2Ref}
+              onChange={e => {
+                linkedPicker1Ref.current.datetimepicker("maxDate", e.date);
+              }}
+            />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5, 6, 9-21">
             {`import React, { useRef } from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -231,17 +321,23 @@ export default () => {
 };`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Custom Icons</h2>
-          <DateTimePicker
-            iconClassName="fa-birthday-cake"
-            icons={{
-              date: "fa fa-birthday-cake",
-              time: "fa fa-hourglass-half",
-              up: "fa fa-chevron-circle-up",
-              down: "fa fa-chevron-circle-down",
-            }}
-          />
+          <h2 id="custom-icons">
+            <a href="#custom-icons">Custom Icons</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker
+              iconClassName="fa-birthday-cake"
+              icons={{
+                date: "fa fa-birthday-cake",
+                time: "fa fa-hourglass-half",
+                up: "fa fa-chevron-circle-up",
+                down: "fa fa-chevron-circle-down",
+              }}
+            />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5-13">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
@@ -259,15 +355,21 @@ export default () => (
 );`}
           </PrismCode>
         </div>
+        <hr />
+
         <div>
-          <h2>Event Listeners</h2>
-          <DateTimePicker
-            onHide={e => console.log("onHide", e)}
-            onShow={e => console.log("onShow", e)}
-            onChange={e => console.log("onChange", e)}
-            onError={e => console.log("onError", e)}
-            onUpdate={e => console.log("onUpdate", e)}
-          />
+          <h2 id="event-listeners">
+            <a href="#event-listeners">Event Listeners</a>
+          </h2>
+          <div className={styles.picker}>
+            <DateTimePicker
+              onHide={e => console.log("onHide", e)}
+              onShow={e => console.log("onShow", e)}
+              onChange={e => console.log("onChange", e)}
+              onError={e => console.log("onError", e)}
+              onUpdate={e => console.log("onUpdate", e)}
+            />
+          </div>
           <PrismCode codeClassName="language-jsx" lineHighlight="2, 5-11">
             {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
