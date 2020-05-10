b5ac28dd380db5dd3fa6ecd5a60d9fa3e2b961d0
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-05-10 19:51:49 +0200

    Modified event listeners example

--- example/src/examples/EventListenersExample.js
+++ example/src/examples/EventListenersExample.js
@@ -15,24 +15,40 @@ export default function EventListenersExample({ noHr = false }) {
         </h2>
         <div className={styles.picker}>
           <DateTimePicker
-            onHide={e => console.log("onHide", e)}
+            onHide={e => console.log("onHide", e.date, e)}
             onShow={e => console.log("onShow", e)}
-            onChange={e => console.log("onChange", e)}
-            onError={e => console.log("onError", e)}
-            onUpdate={e => console.log("onUpdate", e)}
+            onChange={e => console.log("onChange", e.date, e.oldDate, e)}
+            onError={e => console.log("onError", e.date, e.oldDate, e)}
+            onUpdate={e => {
+              console.log(
+                "onUpdate",
+                e.change,
+                e.viewDate,
+                e.viewDate.format("YYYY-MM-DD HH:mm:ss"),
+                e
+              );
+            }}
           />
         </div>
-        <PrismCode language="jsx" lineHighlight="2, 5-11">
+        <PrismCode language="jsx" lineHighlight="2, 5-19">
           {`import React from "react";
 import { DateTimePicker } from "react-tempusdominus-bootstrap";
 
 export default () => (
   <DateTimePicker
-    onHide={e => console.log("onHide", e)}
+    onHide={e => console.log("onHide", e.date, e)}
     onShow={e => console.log("onShow", e)}
-    onChange={e => console.log("onChange", e)}
-    onError={e => console.log("onError", e)}
-    onUpdate={e => console.log("onUpdate", e)}
+    onChange={e => console.log("onChange", e.date, e.oldDate, e)}
+    onError={e => console.log("onError", e.date, e.oldDate, e)}
+    onUpdate={e => {
+      console.log(
+        "onUpdate",
+        e.change,
+        e.viewDate,
+        e.viewDate.format("YYYY-MM-DD HH:mm:ss"),
+        e
+      );
+    }}
   />
 );`}
         </PrismCode>
