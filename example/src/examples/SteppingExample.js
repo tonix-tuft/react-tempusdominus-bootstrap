53f12e9e065ea96fa2ea8689bba1f4e0ce19ec23
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-05-19 23:19:16 +0200

    Added stepping example

---
+++ example/src/examples/SteppingExample.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { DateTimePicker } from "react-tempusdominus-bootstrap";
+import PrismCode from "../code/PrismCode";
+import styles from "../App.module.scss";
+
+export default function SteppingExample({ noHr = false }) {
+  return (
+    <>
+      <div>
+        <h2 id="stepping">
+          <a className={styles.fragmentLink} href="#stepping">
+            Stepping
+          </a>
+        </h2>
+        <div className={styles.picker}>
+          <DateTimePicker stepping={4} />
+        </div>
+        <PrismCode language="jsx" lineHighlight="2, 5">
+          {`import React from "react";
+import { DateTimePicker } from "react-tempusdominus-bootstrap";
+
+export default () => (
+  <DateTimePicker stepping={4} />
+);`}
+        </PrismCode>
+      </div>
+      {noHr || <hr />}
+    </>
+  );
+}
