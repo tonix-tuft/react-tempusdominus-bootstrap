e6564193726d40fc4c2fe287a2f294ce9cc716b8
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2021-05-31 16:31:13 +0200

    Updated copyright notice

--- src/helpers/getPicker.js
+++ src/helpers/getPicker.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Anton Bagdatyev (Tonix)
+ * Copyright (c) 2021 Anton Bagdatyev (Tonix)
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
